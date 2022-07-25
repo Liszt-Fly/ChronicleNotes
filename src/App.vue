203609187fd3916f5a3e58d96710fd384ad79fc8
forestlet <hyf2021@outlook.com>
2022-07-25 14:00:38 +0800

    优化样式、格式化代码

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,22 @@
 import ControlBar from "@/components/common/ControlBar/ControlBar.vue";
 import TabBar from "@/components/common/TabBar.vue";
 
-const fsp =require('fs-extra')
+const fsp = require('fs-extra')
 
 </script>
 
 <template>
   <control-bar></control-bar>
-  <div class="main" spellcheck="false" >
-   <tab-bar></tab-bar>
-    <div class=" article">
-      <router-view v-slot="{ Component }">
-        <keep-alive>
-          <component :is="Component" />
-        </keep-alive>
-      </router-view>
+  <div class="main" spellcheck="false">
+    <tab-bar></tab-bar>
+    <div class="article">
+      <el-scrollbar>
+        <router-view v-slot="{ Component }">
+          <keep-alive>
+            <component :is="Component" />
+          </keep-alive>
+        </router-view>
+      </el-scrollbar>
     </div>
   </div>
 </template>
